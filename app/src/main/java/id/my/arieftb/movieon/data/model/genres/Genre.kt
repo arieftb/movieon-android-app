a96ec70befaedc8562f6a996c6683696502ab1d3
arieftb <arieftb22@gmail.com>
2021-10-26 17:32:01 +0700

    1.0.0-feat(response) : add response for genre collection

---
+++ app/src/main/java/id/my/arieftb/movieon/data/model/genres/Genre.kt
@@ -0,0 +1,6 @@
+package id.my.arieftb.movieon.data.model.genres
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/id/my/arieftb/movieon/data/model/genres/GenreCollectionResponse.kt
@@ -0,0 +1,5 @@
+package id.my.arieftb.movieon.data.model.genres
+
+data class GenreCollectionResponse(
+    val genres: List<Genre>
+)
